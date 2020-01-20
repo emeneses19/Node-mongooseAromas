740a6c8c80ccad6593d8b9977cb79d16d6945352
Meneses Tipe Epifania <menesetaipe19@gmail.com>
2020-01-20 14:11:34 -0500

    version v0.0.1

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.get('/', function(req, res) {
+    res.json('Hello World gggg')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
